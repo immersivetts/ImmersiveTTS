861e30d35386a3cbc2c1abbd0c9c4479ff2c5a0d
anonymous <253237793+immersivetts@users.noreply.github.com>
2026-01-22 11:36:09 +0900

    update code

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 export TORCH_DISTRIBUTED_DEBUG=DETAIL
 CUDA_VISIBLE_DEVICES=0,1 accelerate launch \
-        --config_file='configs/accelerator_config.yaml' train.py \
-        --config='configs/immersive_tts.yaml' 
+        --config_file='configs/accelerator.yaml' train.py \
+        --config='configs/model.yaml' 
