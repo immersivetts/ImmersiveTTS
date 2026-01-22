cc5d47d3adaffaeae38b17255374314f44d87dd9
anonymous <253237793+immersivetts@users.noreply.github.com>
2026-01-22 11:25:47 +0900

    update code

--- inference.sh
+++ inference.sh
@@ -1,5 +1,5 @@
 export TORCH_DISTRIBUTED_DEBUG=DETAIL
-CUDA_VISIBLE_DEVICES=3 python inference_tts.py --config configs/inference_repa.yaml \
+CUDA_VISIBLE_DEVICES=3 python inference.py --config configs/inference_repa.yaml \
             --ckpt_path /your/path/to/checkpoint.pt \
             --output_dir /your/path/to/output_dir \
             --num_inference_steps 25 \
